bb455fdc5a3d959ef3d398331bca1f372c15d556
Ndeye Ndoumbe Ndiaye <ndeyendoumben00@gmail.com>
2026-08-26 23:35:20 +0000

    Add whitenoise for static files

--- Sante_Virtuelle/Sante_Virtuelle/settings.py
+++ Sante_Virtuelle/Sante_Virtuelle/settings.py
@@ -50,6 +50,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -142,6 +143,7 @@ else:
 # Custom user model
 AUTH_USER_MODEL = 'sante_app.User'
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
 
