106ccd279544aa44bc27954554e9e4da7223d046
Ndeye Ndoumbe Ndiaye <ndeyendoumben00@gmail.com>
2026-08-26 23:24:18 +0000

    Fix ASGI app initialization order

--- Sante_Virtuelle/Sante_Virtuelle/asgi.py
+++ Sante_Virtuelle/Sante_Virtuelle/asgi.py
@@ -1,27 +1,22 @@
 """
 ASGI config for Sante_Virtuelle project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
 """
-
 import os
 from dotenv import load_dotenv
 
-# Load environment variables from .env file
 load_dotenv()
 
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Sante_Virtuelle.settings')
+
 from django.core.asgi import get_asgi_application
+django_asgi_app = get_asgi_application()
+
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 from sante_app import routing
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Sante_Virtuelle.settings')
-
 application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
+    "http": django_asgi_app,
     "websocket": AuthMiddlewareStack(
         URLRouter(
             routing.websocket_urlpatterns
